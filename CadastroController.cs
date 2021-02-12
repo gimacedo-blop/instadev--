aa4c4f70a2148a3831b50c748e9cfbc513c9e788
gimacedo-blop <76495661+gimacedo-blop@users.noreply.github.com>
2021-02-12 11:42:47 -0300

    Add files via upload

---
+++ CadastroController.cs
@@ -0,0 +1,12 @@
+namespace Instadev.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View ();
+
+        }
+        
+    }
+}
